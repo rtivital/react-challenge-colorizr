c48aa74fc7ab434c993c6a622865a28f9bb986ba
rtivital <rtivital@gmail.com>
2016-04-27 12:55:04 +0300

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
