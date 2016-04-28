541b7f733f959884face12ab041ccfcc28c47767
rtivital <rtivital@gmail.com>
2016-04-29 00:33:07 +0300

    deploy improvements

--- deploy.js
+++ deploy.js
@@ -3,10 +3,14 @@
 var ghPages = require('gh-pages');
 var path = require('path');
 
+// Module colors (https://github.com/marak/colors.js/) just for UX
+var colors = require('colors');
+
 var config = {
   message: 'Publish Colorizr App'
 };
 
 ghPages.publish(path.join(__dirname, 'public'), config, function(err) {
   if (err) { console.log(err); }
+  console.log('Successfully Deployed to Github Pages'.green.bold);
 });
