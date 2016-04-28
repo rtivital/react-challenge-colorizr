f3f5681bae6fc63167ae872479bfc00d818c2b20
rtivital <rtivital@gmail.com>
2016-04-28 19:13:10 +0300

    add autoprefixer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,20 +3,24 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: './src/index',
+  
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
+
   plugins: [
     new ExtractTextPlugin('style.css'),
     new webpack.optimize.UglifyJsPlugin({
         compressor: { warnings: false }
     })
   ],
+
   module: {
     loaders: [
       {
@@ -26,8 +30,10 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass')
+        loader: ExtractTextPlugin.extract('css!sass!postcss')
       }
-  ]
-  }
+    ]
+  },
+
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
 };
