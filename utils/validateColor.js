094995df0bf2fcdaad889ee56c4d363116ff7fb7
rtivital <rtivital@gmail.com>
2016-05-13 15:33:17 +0300

    finished howto

--- utils/validateColor.js
+++ utils/validateColor.js
@@ -4,7 +4,7 @@
 var testRegex = {
   // hex without `#` is not valid, sorry, that's my rule
   hex: /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i,
-  // can't even imagine how that rgb is constructed
+  // can't even imagine how that re is constructed
   // but it works just fine, I found it here http://goo.gl/javj9B
   rgb: /rgb\(\s*(?:(?:\d{1,2}|1\d\d|2(?:[0-4]\d|5[0-5]))\s*,?){3}\)$/
 };
