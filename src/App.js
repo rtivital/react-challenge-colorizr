f77bb4921885f7f6689442b61d14f66c6ed26f2a
rtivital <rtivital@gmail.com>
2016-08-18 00:33:56 +0300

    remove old counter example

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Counter from './components/Counter/Counter';
-
-export default () => (
-  <div className="container">
-    <h1 className="title"> Basic Counter Example</h1>
-    <Counter />
-  </div>
-);
