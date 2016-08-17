0e69cde5b34e04b247d29f90d467f31a092586a4
rtivital <rtivital@gmail.com>
2016-08-17 22:28:12 +0300

    update webpack devserver options

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+'use strict';
 
-new WebpackDevServer(webpack(config), {
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config');
+const colors = require('colors');
+
+const PORT = 3002;
+
+const serverConfig = {
   contentBase: './public',
   publicPath: '/',
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) { return console.log(err); }
-  console.log('Listening at http://localhost:3000/');
-});
+  historyApiFallback: true,
+};
+
+new WebpackDevServer(webpack(webpackConfig), serverConfig)
+  .listen(PORT, 'localhost', err => {
+    err && console.log(err);
+    console.log(`Listening at ${`http://localhost:${PORT}/`.green}`);
+  });
