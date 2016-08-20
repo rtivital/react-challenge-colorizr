36b9cc89a4f9bfebf5343fd9b478f90c720c2d77
rtivital <rtivital@gmail.com>
2016-08-20 15:19:28 +0300

    update deploy options

--- utils/deploy.js
+++ utils/deploy.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var ghPages = require('gh-pages');
-var path = require('path');
+const path = require('path');
+const ghpages = require('gh-pages');
+const colors = require('colors');
 
-// Module colors (https://github.com/marak/colors.js/) just for UX
-var colors = require('colors');
+ghpages.publish(path.join(__dirname, '../public'), err => {
+  if (err) {
+    console.log(err);
+    return null;
+  }
 
-var config = {
-  message: 'Publish Colorizr App'
-};
-
-ghPages.publish(path.join(__dirname, '../', 'public'), config, function(err) {
-  if (err) { console.log(err); }
-  console.log('Successfully Deployed to Github Pages'.green.bold);
+  console.log('Successfully deployed to gh-pages'.green);
 });
