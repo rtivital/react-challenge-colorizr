35dc001709e9cf138e7251efbd3a628cdff419a8
rtivital <rtivital@gmail.com>
2016-08-20 15:16:41 +0300

    use right router history only when required

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-// Router things
-import { browserHistory, hashHistory } from 'react-router';
-import AppRouter from 'app/routes';
-
-// Redux things
 import { Provider } from 'react-redux';
-import configureStore from 'app/store';
 
-// React Fastclick removes 400ms delay on touch devices
-// https://github.com/JakeSidSmith/react-fastclick
 import 'react-fastclick';
 
-import 'app/styles/index.scss';
+import configureStore from './store';
+import AppRouter from './routes';
+import './styles/index.scss';
 
 const store = configureStore();
 
-// Use hashHistory while deploying on gh-pages otherwize prefer browserHistory
-const history = process.env.BUILD === 'pages' ? hashHistory : browserHistory;
+// Minification process will remove browser or hash history depending on build type
+/* eslint-disable global-require */
+if (process.env.BUILD === 'pages') {
+  // hashHistory will be used while deploying on gh-pages
+  const hashHistory = require('react-router/lib/hashHistory');
 
-render(
-  <Provider store={store}>
-    <AppRouter history={history} />
-  </Provider>,
-  document.getElementById('app')
-);
+  render(
+    <Provider store={store}>
+      <AppRouter history={hashHistory} />
+    </Provider>,
+    document.getElementById('app')
+  );
+} else {
+  // browserHistory will be used for any other builds
+  const browserHistory = require('react-router/lib/browserHistory');
+
+  render(
+    <Provider store={store}>
+      <AppRouter history={browserHistory} />
+    </Provider>,
+    document.getElementById('app')
+  );
+}
