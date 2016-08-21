a5a70cd0aeb43a6e56aee4d7d38da441694ddd84
rtivital <rtivital@gmail.com>
2016-08-21 18:01:58 +0300

    add color panel styles

--- src/components/ColorDisplay/ColorDisplay.jsx
+++ src/components/ColorDisplay/ColorDisplay.jsx
@@ -1,13 +1,32 @@
 import React, { PropTypes } from 'react';
 import color from 'color';
+import { v4 } from 'node-uuid';
+
+const Chanel = ({ value, name }) => (
+  <div className="chanel">
+    <span className="chanel__name">{name.toUpperCase()}</span>
+    <span className="chanel__value">{value}</span>
+  </div>
+);
+
+Chanel.propTypes = {
+  value: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 const ColorDisplay = ({ colorValue }) => {
   const transformedColor = color(colorValue);
   const hex = transformedColor.hexString();
+  const rgb = transformedColor.rgb();
+
+  const chanels = Object.keys(rgb).map(
+    chanel => <Chanel value={rgb[chanel]} name={chanel} key={v4()} />
+  );
 
   return (
-    <div className="color-display" style={{ backgroundColor: hex }}>
-      Color display
+    <div className="color-display">
+      <div className="color-display__display" style={{ backgroundColor: hex }} />
+      <div className="color-display__chanels">{chanels}</div>
     </div>
   );
 };
