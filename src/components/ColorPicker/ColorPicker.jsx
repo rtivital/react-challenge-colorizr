896355440f2a0fa3eb054f46560c525a1163deaf
rtivital <rtivital@gmail.com>
2016-08-27 17:17:47 +0300

    update colorpicker to display current value

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,21 +1,38 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ColorPickerComponent from 'react-color-picker';
+import { ColorDisplay } from 'app/components/ColorDisplay';
 import 'react-color-picker/index.css';
 
-const ColorPicker = ({ defaultColor, onChange }) => (
-  <div className="color-picker">
-    <ColorPickerComponent
-      onDrag={onChange}
-      defaultColor={defaultColor}
-      saturationWidth={300}
-      hueWidth={40}
-    />
-  </div>
-);
+export default class ColorPicker extends Component {
+  static propTypes = {
+    defaultColor: PropTypes.string,
+    onChange: PropTypes.func,
+  }
 
-ColorPicker.propTypes = {
-  defaultColor: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-};
+  static defaultProps = {
+    defaultColor: '#000',
+    onChange: v => v,
+  }
 
-export default ColorPicker;
+  state = { currentColor: this.props.defaultColor }
+
+  handleColorSelection = (newColor) => {
+    this.setState({ currentColor: newColor });
+    this.props.onChange(newColor);
+  }
+
+  render() {
+    return (
+      <div className="color-picker">
+        <ColorPickerComponent
+          onDrag={this.handleColorSelection}
+          defaultColor={this.props.defaultColor}
+          saturationWidth={300}
+          hueWidth={40}
+        />
+
+        <ColorDisplay colorValue={this.state.currentColor} />
+      </div>
+    );
+  }
+}
