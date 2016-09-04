0a04d315a5c683473d7c1d51887c248771355e23
rtivital <rtivital@gmail.com>
2016-09-04 15:53:41 +0300

    fix indexpage alignment and eslint errors

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import { ColorPicker } from 'app/containers';
 import { connect } from 'react-redux';
 import color from 'color';
@@ -7,29 +7,34 @@ import './index-page.scss';
 
 @connect(state => ({ lead: state.color.lead }))
 export default class IndexPage extends PureComponent {
+  static propTypes = {
+    lead: PropTypes.string.isRequired,
+  }
+
   render() {
     const luminosity = color(this.props.lead).luminosity();
     const titlesClassName = cx('header__titles', {
       'header__titles--dark': luminosity >= 0.5,
       'header__titles--light': luminosity < 0.5,
     });
+
     return (
       <div className="page">
-      <header className="header" style={{ backgroundColor: this.props.lead }}>
-        <div className="header__inner">
-          <div className={titlesClassName}>
-          <h1 className="header__title">
-            The last color service you will ever need
-          </h1>
-          <h2 className="header__subtitle">
-            Just pick one color and let the magic happen
-          </h2>
+        <header className="header" style={{ backgroundColor: this.props.lead }}>
+          <div className="header__inner">
+            <div className={titlesClassName}>
+              <h1 className="header__title">
+                The last color service you will ever need
+              </h1>
+              <h2 className="header__subtitle">
+                Just pick one color and let the magic happen
+              </h2>
+            </div>
+            <div className="header__color-picker">
+              <ColorPicker />
+            </div>
           </div>
-          <div className="header__color-picker">
-            <ColorPicker />
-          </div>
-        </div>
-      </header>
+        </header>
       </div>
     );
   }
