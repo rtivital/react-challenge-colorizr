24306f0203c89a40ca6957c8dcead419854bc717
rtivital <rtivital@gmail.com>
2016-09-04 15:53:13 +0300

    add store serialization

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import multiMiddleware from 'redux-multi';
 import { routerMiddleware } from 'react-router-redux';
+import throttle from 'lodash.throttle';
 import rootReducer from 'app/reducers';
 
 // create routing actions for hashHistory while deploying on gh-pages
@@ -18,16 +19,44 @@ export const middlewares = [
   routingMiddleware,
 ];
 
-const configureStore = initialState => {
-  // Prevent redux devTools initialization in production
-  if (process.env.NODE_ENV === 'development') {
-    return createStore(rootReducer, initialState, compose(
-      applyMiddleware(...middlewares),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
-    ));
+function loadState() {
+  try {
+    const serializedState = JSON.parse(localStorage.getItem('state'));
+    if (serializedState === null) { return undefined; }
+    return serializedState;
+  } catch (e) {
+    return undefined;
   }
+}
 
-  return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
+function saveState(state) {
+  try {
+    // remove routing before saving: it ruins everything!
+    const stateToSave = { ...state };
+    delete stateToSave.routing;
+
+    localStorage.setItem('state', JSON.stringify(stateToSave));
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+const configureStore = (initialState = loadState()) => {
+  // Prevent redux devTools initialization in production
+  const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middlewares),
+    window.devToolsExtension && process.env.NODE_ENV === 'development'
+      ? window.devToolsExtension()
+      : f => f
+  ));
+
+  // Save state to localStorage every second
+  store.subscribe(throttle(() => {
+    saveState(store.getState());
+  }, 1000));
+
+  return store;
 };
 
 export default configureStore;
