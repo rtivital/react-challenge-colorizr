aca7df11c6f7bb4e680c8b854878d5a732d20367
rtivital <rtivital@gmail.com>
2016-09-06 01:08:22 +0300

    add gradient and info toggle

--- src/components/ColorDisplayGroup/ColorDisplayGroup.jsx
+++ src/components/ColorDisplayGroup/ColorDisplayGroup.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { v4 } from 'node-uuid';
+import cx from 'classnames';
 import { Button, Checkbox } from 'app/ui';
 import { ColorDisplay } from 'app/components';
 import './color-display-group.scss';
@@ -10,22 +11,49 @@ export default class ColorDisplayGroup extends Component {
     title: PropTypes.string.isRequired,
   }
 
-  state = {}
+  state = {
+    gradient: false,
+    info: false,
+  }
+
+  toggleGradient = () => {
+    this.setState({ gradient: !this.state.gradient });
+  }
+
+  toggleInfo = () => {
+    this.setState({ info: !this.state.info });
+  }
 
   render() {
     const colorDisplays = this.props.colors.map(
-      (color) => <ColorDisplay colorValue={color} key={v4()} />
+      (color) => <ColorDisplay colorValue={color} hideInfo={!this.state.info} key={v4()} />
     );
+
+    const displaysClassName = cx('color-display-group__displays', {
+      'color-display-group__displays--gradient': this.state.gradient,
+    });
+
+    const gradient = `linear-gradient(to right, ${this.props.colors[0]}, ${this.props.colors[this.props.colors.length - 1]})`;
 
     return (
       <div className="color-display-group">
         <h3 className="color-display-group__title">{this.props.title}</h3>
-        <div className="color-display-group__displays">
+        <div className={displaysClassName} style={{ backgroundImage: this.state.gradient ? gradient : '' }}>
           {colorDisplays}
         </div>
         <div className="color-display-group__controls">
-          <Checkbox className="color-display-group__control" label="Show Info" />
-          <Checkbox className="color-display-group__control" label="Gradient" />
+          <Checkbox
+            className="color-display-group__control"
+            label="Show Info"
+            checked={this.state.info}
+            onChange={this.toggleInfo}
+          />
+          <Checkbox
+            className="color-display-group__control"
+            label="Gradient"
+            checked={this.state.gradient}
+            onChange={this.toggleGradient}
+          />
           <Button className="color-display-group__control">Select All</Button>
           <Button className="color-display-group__control" theme="red">Remove All</Button>
         </div>
