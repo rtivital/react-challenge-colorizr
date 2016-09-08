46db18e2f75b1dfb7f95dc259cfbd1b281c75554
rtivital <rtivital@gmail.com>
2016-09-08 23:31:20 +0300

    styles refactoring

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,6 @@ const Navbar = (props, context) => {
   return (
     <nav className="navbar">
       <Container className="navbar__inner">
-        <div className="narbar__logo">
-          <Logo />
-        </div>
-
         <div className="navbar__navigation">
           <NavbarLink index className="navbar__link" to="/">Create</NavbarLink>
           <NavbarLink className="navbar__link" to="/explore">Explore</NavbarLink>
