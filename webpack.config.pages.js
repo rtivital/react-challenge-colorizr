dd91522f15b265bfe576d6ee30081369c5a080e9
rtivital <rtivital@gmail.com>
2016-09-09 00:12:22 +0300

    fix deploy config

--- webpack.config.pages.js
+++ webpack.config.pages.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/colorizer/',
+    publicPath: '/react-challenge-colorizr/',
   },
 
   resolve: {
