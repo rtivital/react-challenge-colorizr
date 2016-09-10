b61a110f9e241ac0ac4259c003af9322c14582b4
rtivital <rtivital@gmail.com>
2016-09-11 01:35:41 +0300

    add mergeHex function and more docs

--- src/lib/colors/hex.js
+++ src/lib/colors/hex.js
@@ -1,5 +1,16 @@
 /** @module HEX */
 
+/**
+ * validateHex - Utility function for HEX color validation that throws errors while development
+ * 
+ * @param {string} value - value to test
+ *
+ * @example
+ * validateHex('#fff');     // it's ok
+ * validateHex('#00000');   // TypeError
+  * isUnprefixedHex('zzz'); // TypeError
+ * isUnprefixedHex();       // TypeError
+ */
 function validateHex(value) {
   if (!isHex(value) && process.env.NODE_ENV === 'development') {
     throw new TypeError(`Recieved value ${value} is not a valid HEX color`);
@@ -7,7 +18,8 @@ function validateHex(value) {
 }
 
 /**
- * Tests if provided HEX color does not contain hash
+ * isUnprefixedHex - Tests if provided HEX color does not contain hash
+ *
  * @param {string} value - HEX color to test
  * @return {boolean}
  *
@@ -22,7 +34,8 @@ export function isUnprefixedHex(value) {
 }
 
 /**
- * Tests if provided HEX color does contain hash
+ * isPrefixedHex - Tests if provided HEX color does contain hash
+ *
  * @param {string} value - HEX color to test
  * @return {boolean}
  *
@@ -37,7 +50,8 @@ export function isPrefixedHex(value) {
 }
 
 /**
- * Tests if provided color value is HEX color, both xxx and #xxx values are accepted
+ * isHex -Tests if provided color value is HEX color, both xxx and #xxx values are accepted
+ *
  * @param {string} value
  * @return {boolean}
  *
@@ -53,7 +67,8 @@ export function isHex(value) {
 }
 
 /**
- * Used to unprefix HEX color value (remove hash character at position 0)
+ * unprefixHex - Used to unprefix HEX color value (remove hash character at position 0)
+ *
  * @param {string} value - HEX color with (#fff) or without hash (fff)
  * @return {string} hex color value formated like fff or fff
  *
@@ -70,7 +85,8 @@ export function unprefixHex(value) {
 }
 
 /**
- * Used to prefix HEX color value (add hash character to position 0)
+ * prefixHex - Used to prefix HEX color value (add hash character to position 0)
+ *
  * @param {string} value - HEX color with (#fff) or without hash (fff)
  * @return {string} hex color value formated like fff or #fff
  *
@@ -87,7 +103,8 @@ export function prefixHex(value) {
 }
 
 /**
- * Creates long hex value from provided HEX color
+ * createLongHex - Creates long hex value from provided HEX color
+ *
  * @param {string} value - color to work with
  * @param {boolean} [prefixed=false] - passed if value should be prefixed
  * @return {string} HEX color with format ffffff or #ffffff
@@ -110,7 +127,8 @@ export function createLongHex(value, prefixed = false) {
 }
 
 /**
- * Splits hex color to separate chanels (r, g, b)
+ * splitHex - Splits hex color to separate chanels (r, g, b)
+ *
  * @param {string} value - color to work with
  * @param {boolean} [splitType='object'] - array or object split type
  * @return {Object|Array} - splitted hex value
@@ -124,7 +142,7 @@ export function createLongHex(value, prefixed = false) {
  */
 export function splitHex(color, splitType = 'object') {
   validateHex(color);
-  const hex = unprefixHex(createLongHex(color));
+  const hex = createLongHex(color);
   const chanels = [];
 
   for (let i = 0; i < hex.length; i += 2) {
@@ -134,4 +152,42 @@ export function splitHex(color, splitType = 'object') {
   return splitType === 'object'
     ? { r: chanels[0], g: chanels[1], b: chanels[2] }
     : chanels;
+}
+
+/**
+ * chanelToHex - Turns decimal chanel value to hex
+ *
+ * @param  {number} chanel - red, green or blue chanel
+ * @return {string}
+ *
+ * @example
+ * chanelToHex(255); // 'ff'
+ * chanelToHex(15);  // '0f'
+ * chanelToHex(0);   // '00'
+ */
+export function chanelToHex(chanel) {
+  const value = chanel.toString(16);
+  return value.length === 2 ? value : `0${value}`;
+}
+
+
+/**
+ * mergeHex - Merges splited hex value to string
+ *
+ * @param  {Array|Object} value - object with keys r, g, b or array with three numbers
+ * @param  {boolean} [prefix = true] - passed if hex should be prefixed
+ * @return {string} - HEX string
+ */
+export function mergeHex(value, prefix = true) {
+  let color = '';
+
+  if (Array.isArray(value)) {
+    color = value.map(chanelToHex).join('');
+  } else if (typeof value === 'object' && value !== null) {
+    color += chanelToHex(value.r);
+    color += chanelToHex(value.g);
+    color += chanelToHex(value.b);
+  }
+
+  return prefix ? `#${color}` : color;
 }
