7413635933c2a65401e14eb2d1130c3d823bcfe7
rtivital <rtivital@gmail.com>
2016-09-15 23:24:15 +0300

    fix Colorizr library test

--- src/tests/lib/Colorizr.test.js
+++ src/tests/lib/Colorizr.test.js
@@ -7,8 +7,8 @@ const validColors = ['#fff', 'ccc', { r: 3, g: 56, b: 167 }, [34, 78, 56]];
 test('Colorizr constructor tests', (t) => {
   validColors.forEach((validColor) => {
     const instance = new Colorizr(validColor);
-    t.equal(
-      Object.keys(instance).every((key) => chanels.some((chanel) => chanel === key)), true,
+    t.deepEqual(
+      Object.keys(instance.color).sort(), chanels.sort(),
       `Expected Colorizr to produce splitted color value with ${chanels.join(', ')} keys`
     );
   });
