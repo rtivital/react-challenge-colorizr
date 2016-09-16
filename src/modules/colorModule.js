ca207ce8b26a3d6a06e03da5d82ae4431f9c615b
rtivital <rtivital@gmail.com>
2016-09-17 01:14:41 +0300

    add right luminosity calculations to color module

--- src/modules/colorModule.js
+++ src/modules/colorModule.js
@@ -1,37 +1,65 @@
 import { createAction, handleActions } from 'redux-actions';
-import color from 'color';
-import { hex } from 'lib';
+import { Colorizr } from 'lib';
+
+const INITIAL_LEAD_COLOR = '#0C93D2';
+const INITIAL_MIXED_COLOR = '#F44336';
+
+function getLuminosityGroup(value) {
+  const color = new Colorizr(value);
+  const darkened = [];
+  const lightened = [];
+  let enableLight = true;
+  let enableDark = true;
+
+  for (let i = 1; i <= 5; i += 1) {
+    const lightenedColor = color.clone().lighten(i * 10);
+    const darkenedColor = color.clone().darken(i * 10);
+
+    if (lightenedColor.luminosity() === 1) { enableLight = false; }
+    if (darkenedColor.luminosity() === 0) { enableDark = false; }
+
+    if (enableLight) { lightened.push(lightenedColor.hex()); }
+    if (enableDark) { darkened.push(darkenedColor.hex()); }
+  }
+
+  for (let i = 1, l = 10 - (lightened.length + darkened.length); i <= l; i++) {
+    if (enableLight) {
+      const lightenedColor = color.clone().lighten((i + 5) * 10);
+      lightened.push(lightenedColor.hex());
+    }
+
+    if (enableDark) {
+      const darkenedColor = color.clone().darken((i + 5) * 10);
+      darkened.push(darkenedColor.hex());
+    }
+  }
+
+  return lightened.reverse().concat(darkened);
+}
+
+function getMixedGroup(value) {
+  return [];
+}
 
 export const actions = {
-  setLeadColor: createAction('SET_LEAD_COLOR'),
+  setLeadColor: createAction('COLOR/SET_LEAD_COLOR'),
+  setMixedColor: createAction('COLOR/SET_MIXED_COLOR'),
 };
 
 const initialState = {
-  lead: '#0c93d2',
-  mixer: '#f5f',
-  luminosity: [],
+  lead: INITIAL_LEAD_COLOR,
+  mixer: INITIAL_MIXED_COLOR,
+  luminosityGroup: getLuminosityGroup(INITIAL_LEAD_COLOR),
+  mixedGroup: getMixedGroup(INITIAL_LEAD_COLOR, INITIAL_MIXED_COLOR),
 };
 
 export const reducer = handleActions({
   [actions.setLeadColor](state, { payload }) {
-    if (!hex.isHex(payload)) { return state; }
-    const value = color(hex.prefixHex(payload));
-    const luminosity = value.luminosity();
-    const lightColors = parseInt(luminosity * 10, 10);
-    const darkColors = 10 - lightColors;
-    const darkened = [];
-    const lightened = [];
-
-    for (let i = 0; i < lightColors; i += 1) {
-      lightened.push(value.clone().lighten(i * 0.1).hexString());
-    }
-
-    for (let i = 0; i < darkColors; i += 1) {
-      darkened.push(value.clone().darken(i * 0.1).hexString());
-    }
-
-    const lum = lightened.reverse().concat(darkened);
-
-    return { ...state, lead: hex.prefixHex(payload), luminosity: lum };
+    if (!Colorizr.isHex(payload)) { return state; }
+    return {
+      ...state,
+      lead: Colorizr.prefixHex(payload),
+      luminosity: getLuminosityGroup(payload),
+    };
   },
 }, initialState);
