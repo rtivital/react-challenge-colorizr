1789712adab2d7b02665ce9692aad09471706182
rtivital <rtivital@gmail.com>
2016-09-17 23:58:34 +0300

    fix luminosity group component error

--- src/containers/LuminosityGroup.jsx
+++ src/containers/LuminosityGroup.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { ColorDisplayGroup } from 'components';
 
-@connect(state => ({ colors: state.color.luminosity }))
+@connect(state => ({ colors: state.color.luminosityGroup }))
 export default class LuminosityGroupContainer extends PureComponent {
   static propTypes = {
     colors: PropTypes.array.isRequired,
