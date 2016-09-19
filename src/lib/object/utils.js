50135f004543a1b3a723dbcab790e19247ccb676
rtivital <rtivital@gmail.com>
2016-09-20 00:22:32 +0300

    add object utils docs

--- src/lib/object/utils.js
+++ src/lib/object/utils.js
@@ -1,8 +1,30 @@
+/**
+ * isObject - Function for checking if provided value is actually an object,
+ *  not array, not function, just an object
+ *
+ * @param  {*} value - any sort of value to test
+ * @return {boolean}
+ *
+ * @example
+ * isObject(null); // false
+ * isObject([]); // false
+ * isObject({}); // true
+ */
 export function isObject(value) {
   const type = typeof value;
   return (type === 'object' || type === 'function') && value !== null && !Array.isArray(value);
 }
 
+/**
+ * isEmpty - Safe function for checking if provided value is an empty object
+ *
+ * @param  {*} value - any value to test
+ * @return {boolean} - checks if value is an object and at least has one key
+ *
+ * @example
+ * isEmpty({}); // true
+ * isEmpty({ a: 1 }); // false
+ */
 export function isEmpty(value) {
   if (!isObject(value)) { return true; }
   return !Object.keys(value).length;
