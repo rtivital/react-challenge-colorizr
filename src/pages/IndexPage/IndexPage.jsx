0a61ec8912d684b2c1738c45aa28a19703c21e60
rtivital <rtivital@gmail.com>
2016-10-08 20:05:37 +0300

    add gradient animation to header

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -2,18 +2,20 @@ import React, { PureComponent, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import color from 'color';
 import cx from 'classnames';
+import { gradient } from 'lib';
 
 import { ColorPicker, LuminosityGroup, MixedGroup } from 'containers';
 import { Container } from 'ui';
 import './index-page.scss';
 
-@connect(state => ({ lead: state.color.lead }))
+@connect(state => ({ lead: state.color.lead, mixer: state.color.mixer }))
 export default class IndexPage extends PureComponent {
   static propTypes = {
     lead: PropTypes.string.isRequired,
   }
 
   render() {
+    const { lead, mixer } = this.props;
     const luminosity = color(this.props.lead).luminosity();
     const titlesClassName = cx('header__titles', {
       'header__titles--dark': luminosity >= 0.5,
@@ -22,7 +24,7 @@ export default class IndexPage extends PureComponent {
 
     return (
       <div className="page index-page">
-        <header className="header" style={{ backgroundColor: this.props.lead }}>
+        <header className="header" style={{ backgroundImage: gradient.createGradient([lead, mixer]) }}>
           <Container className="header__inner">
             <div className={titlesClassName}>
               <h1 className="header__title">
