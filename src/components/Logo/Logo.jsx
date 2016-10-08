ba9ae5c6679554c3cd8825a108fa980b3aef414d
rtivital <rtivital@gmail.com>
2016-10-08 15:56:52 +0300

    add new logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import IndexLink from 'react-router/lib/IndexLink';
-import logoImage from './logo.svg';
 import './logo.scss';
 
-const Logo = () => (
-  <div className="logo">
-    <IndexLink className="logo__link" to="/">
-      <img className="logo__image" src={logoImage} alt="" />
-    </IndexLink>
-  </div>
-);
+const Logo = ({ children, colors }) => {
+  const letters = children.split('').map((letter, index) => (
+    <span
+      className="logo__letter"
+      style={{ color: colors[index] }}
+      key={`${letter}-${index}`}
+    >
+      {letter}
+    </span>
+  ));
+
+  return (
+    <div className="logo">
+      <IndexLink className="logo__link" to="/">
+        {letters}
+      </IndexLink>
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  children: PropTypes.string.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string),
+};
+
+Logo.defaultProps = {
+  children: 'Colorizr',
+  colors: ['#F12509', '#D72031', '#CA1D45', '#BD1B59', '#B0186D', '#A31681', '#961395', '#8911A9'],
+};
 
 export default Logo;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from 'ui';
+import { Logo } from 'components';
 import NavbarLink from './NavbarLink';
 import './navbar.scss';
 
 const Navbar = () => (
   <nav className="navbar">
     <Container className="navbar__inner">
+      <div className="navbar__logo"><Logo /></div>
       <div className="navbar__navigation">
         <NavbarLink index className="navbar__link" to="/">Create</NavbarLink>
         <NavbarLink className="navbar__link" to="/explore">Explore</NavbarLink>
