a63e31fd4af818b423eed9f8d3599aac6c44bf67
rtivital <rtivital@gmail.com>
2016-10-09 13:28:34 +0300

    add selection module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { reducer as color } from './colorModule';
+import { reducer as selection } from './selectionModule';
 
 export default combineReducers({ routing, color });
 
---
+++ src/modules/selectionModule.js
@@ -0,0 +1,20 @@
+import { createAction, handleActions } from 'redux-actions';
+import { hex } from 'lib';
+
+export const actions = {
+  addSelectionColor: createAction('SELECTION/ADD_SELECTION_COLOR'),
+  removeSelectionColor: createAction('SELECTION/REMOVE_SELECTION_COLOR'),
+};
+
+const initialState = [];
+
+export const reducer = handleActions({
+  [actions.addSelectionColor](state, { payload }) {
+    return state.concat(hex.toLongHex(payload, true));
+  },
+
+  [actions.removeSelectionColor](state, { payload }) {
+    const colorToRemove = hex.toLongHex(payload, true);
+    return state.filter((color) => color === !olorToRemove);
+  },
+}, initialState);
