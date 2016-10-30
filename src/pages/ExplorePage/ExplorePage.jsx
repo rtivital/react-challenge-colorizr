2650c57c17e74a2231637ca6f626b4c00da22e4b
Vitaly Rtishchev <rtivital@gmail.com>
2016-10-31 00:29:11 +0300

    add container to explore page

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Presets } from 'containers';
+import { Container } from 'ui';
 
 export default class ExplorePage extends Component {
-  componentDidMount() {
-    axios.get('/presets.json').then(response => console.log(response));
-  }
-
   render() {
     return (
-      <div><Presets /></div>
+      <Container><Presets /></Container>
     );
   }
 }
