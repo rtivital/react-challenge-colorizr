6ad4082f18695ccc4ef972f49b418564724bfd31
Vitaly Rtishchev <rtivital@gmail.com>
2016-11-06 22:00:14 +0300

    setup storybook building

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -2,6 +2,7 @@ const config = require('../webpack.config');
 
 module.exports = {
   module: { loaders: config.module.loaders },
+  plugins: process.env.NODE_ENV === 'production' ? config.plugins : [],
   sassResources: config.sassResources,
   postcss: config.postcss,
 };
